76feb5bc20e31b8446cca7c3e038aef48802a0eb
JulietLima <lijulijuliju@gmail.com>
2014-08-20 22:36:08 -0400

    Ready to go!

--- run_analysis.R
+++ run_analysis.R
@@ -12,19 +12,54 @@ train <- read.table("./train/X_train.txt")
 trainsubject <- read.table("train/subject_train.txt")
 trainy <- read.table("train/y_train.txt")
 
-## Add names
-names(test) <- features$V2
-names(train) <- features$V2
+## Descriptive Variable Names (Step 4)
+names(test) <- make.names(features$V2)
+names(train) <- make.names(features$V2)
 
-## Merge files
+## Merge files (Step 1)
 
 # Merge Test
-test$y <- testy$V1
+test$activities <- testy$V1
 test$subject <-testsubject$V1
 # Merge Train
-train$y <- trainy$V1
+train$activities <- trainy$V1
 train$subject <- trainsubject$V1
 # Merge Test and Train
 combined <- rbind(test,train)
+# Further clean up the names and remove dots
+names(combined) <- gsub("[.]", "", names(combined))
 
-## Measurements
+## Measurements (Step 2)
+# Here I use grep to obtain list of column names with mean and std in them
+# 562 and 563 are the subject and activities columns
+namelist <- c(grep("mean",names(combined)), grep("std",names(combined)), 562, 563)
+combined <- combined[,namelist]
+
+## Descriptive names for activities (Step 3)
+library(car)
+combined$activities <- recode(combined$activities, "1='WALKING';
+                              2='WALKING_UPSTAIRS';
+                              3='WALKING_DOWNSTAIRS';
+                              4='SITTING';5='STANDING';6='LAYING'")
+
+## Create 2nd dataset
+# Need to create a column storing unique identifier for each activities-subject
+combinedtemp <- cbind(combined, paste(combined$activities, combined$subject))
+
+# Initialize new data frame
+combinednew <- data.frame(numeric(length=length(table(combinedtemp[,82]))))
+# Loop across the 79 measurements
+for (i in 1:79) {
+    temp <- tapply(combinedtemp[,i],combinedtemp[,82], mean)
+    combinednew <- cbind(combinednew, temp)
+}
+combinednew[1]<-NULL
+names(combinednew) <- names(combined)[1:79] # Variable names
+combinednew$id <- row.names(combinednew)
+row.names(combinednew) <- NULL
+combinednew$activity <- substring(combinednew$id, 0, nchar(combinednew$id)-2)
+combinednew$subject <- substring(combinednew$id, nchar(combinednew$id)-1)
+combinednew$subject <- as.numeric(combinednew$subject)
+
+## Output 2nd dataset
+write.table(combinednew, file="tidy.txt", row.names=FALSE)
